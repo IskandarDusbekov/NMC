29c9cf9b3c752103502d43545f6c7189874c85e9
Iskandar <iskandardusbekov1@gmail.com>
2026-04-22 23:05:19 +0500

    WhiteNoise
    STATIC_URL static/ emas, /static/ qilindi.
    MEDIA_URL ham /media/ qilindi.
    WhiteNoise qo‘shildi, Passenger hostingda Django static fayllarni bera oladi.
    requirements.txtga whitenoise qo‘shildi.

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = OPTIONAL_APPS + [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,11 +116,12 @@ USE_I18N = True
 USE_TZ = False
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
