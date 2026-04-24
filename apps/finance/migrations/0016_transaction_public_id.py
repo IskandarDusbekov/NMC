f8505fb092b820fcbc0bf1c51832ecd36c6a0570
Iskandar <iskandardusbekov1@gmail.com>
2026-04-24 14:23:56 +0500

    public_id avval null=True qilib qo‘shiladi

--- apps/finance/migrations/0016_transaction_public_id.py
+++ apps/finance/migrations/0016_transaction_public_id.py
@@ -3,6 +3,13 @@ import uuid
 from django.db import migrations, models
 
 
+def fill_transaction_public_ids(apps, schema_editor):
+    Transaction = apps.get_model('finance', 'Transaction')
+    for transaction in Transaction.objects.filter(public_id__isnull=True).iterator():
+        transaction.public_id = uuid.uuid4()
+        transaction.save(update_fields=['public_id'])
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,6 +18,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
+            model_name='transaction',
+            name='public_id',
+            field=models.UUIDField(blank=True, editable=False, null=True),
+        ),
+        migrations.RunPython(fill_transaction_public_ids, migrations.RunPython.noop),
+        migrations.AlterField(
             model_name='transaction',
             name='public_id',
             field=models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
--- apps/objects/migrations/0006_public_ids.py
+++ apps/objects/migrations/0006_public_ids.py
@@ -3,6 +3,19 @@ import uuid
 from django.db import migrations, models
 
 
+def fill_object_public_ids(apps, schema_editor):
+    ConstructionObject = apps.get_model('objects', 'ConstructionObject')
+    WorkItem = apps.get_model('objects', 'WorkItem')
+
+    for construction_object in ConstructionObject.objects.filter(public_id__isnull=True).iterator():
+        construction_object.public_id = uuid.uuid4()
+        construction_object.save(update_fields=['public_id'])
+
+    for work_item in WorkItem.objects.filter(public_id__isnull=True).iterator():
+        work_item.public_id = uuid.uuid4()
+        work_item.save(update_fields=['public_id'])
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -13,9 +26,20 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='constructionobject',
             name='public_id',
-            field=models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
+            field=models.UUIDField(blank=True, editable=False, null=True),
         ),
         migrations.AddField(
+            model_name='workitem',
+            name='public_id',
+            field=models.UUIDField(blank=True, editable=False, null=True),
+        ),
+        migrations.RunPython(fill_object_public_ids, migrations.RunPython.noop),
+        migrations.AlterField(
+            model_name='constructionobject',
+            name='public_id',
+            field=models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
+        ),
+        migrations.AlterField(
             model_name='workitem',
             name='public_id',
             field=models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
